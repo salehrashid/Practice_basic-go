04daa6263cc25e8fbecea745903bd24ba45dca93
salehrashid <saleh.rashid.b@gmail.com>
2023-08-28 16:58:44 +0700

    add arithmetich operator

---
+++ operators/arithmetic-operator.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("penjumlahan", 2+1)
+	fmt.Println("pengurangan", 2-1)
+	fmt.Println("perkalian", 2*1)
+	fmt.Println("pembagian", 2/1)
+	fmt.Println("hasil pembagian", 2%1)
+
+	fmt.Println("======================")
+
+	var a = 1
+	var b = 2
+
+	fmt.Println("hasil penjumlahan dari a dan b adalah", a+b)
+}
