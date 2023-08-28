49cdc4b5a94cab55120990d0bb0a04d8a2992008
salehrashid <saleh.rashid.b@gmail.com>
2023-08-29 05:51:03 +0700

    add constant

---
+++ constant.go
@@ -0,0 +1,16 @@
+package main
+
+func main() {
+	const name = "saleh"
+
+	//hanya sekali saja mendeklarasikan sebuah nilai const
+	//name = "rashid"
+
+	//bisa juga membuat multiple nilai dalam satu const, sama seperti variable
+	const (
+		firstName = ""
+		lastName  = ""
+	)
+
+	println(name)
+}
