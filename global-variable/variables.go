688b89cb0661ada3052cde3d5411aea0cde6bb7e
salehrashid <saleh.rashid.b@gmail.com>
2023-08-29 17:28:49 +0700

    add global var

---
+++ global-variable/variables.go
@@ -0,0 +1,4 @@
+package global_variable
+
+var global = "ini ada var di dalam folder global-variable, selalu diawali dengan huruf kecil"
+var Global = "ini ada var di luar folder global-variable, selalu diawali dengan huruf kapital"
