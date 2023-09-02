4e1d982c641e41fa759d3c3fafe13c3f483aaea6
salehrashid <saleh.rashid.b@gmail.com>
2023-09-02 11:21:13 +0700

    add for loops

---
+++ for-loops.go
@@ -0,0 +1,72 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	println("================ conventional for loops ================")
+
+	for i := 1; i <= 25; i++ {
+		println(i, ":")
+	}
+
+	println("================ print data array with for loops ================")
+
+	namaMotorArray := [3]string{
+		"MV Agusta",
+		"Aprilia",
+		"Kawasaki",
+	}
+
+	for _, value := range namaMotorArray {
+		fmt.Println(value)
+	}
+
+	println("================ print data slice with for loops ================")
+
+	namaMobilSlice := []string{
+		"Brabus",
+		"Mazda",
+		"Supra",
+	}
+
+	for _, value := range namaMobilSlice {
+		fmt.Println(value)
+	}
+
+	println("================ print data map with for loops ================")
+
+	namaPesawatMap := map[string]string{
+		"pesawat1": "Sukhoi Su-35S",
+		"pesawat2": "Dassault Rafale",
+		"pesawat3": "Lockheed Martin F-22 Raptor",
+	}
+
+	for key, value := range namaPesawatMap {
+		fmt.Println(key, "=", value)
+	}
+
+	println("================ angka genap ================")
+
+	for i := 0; i <= 50; i += 2 {
+		println(i)
+	}
+
+	println("================ angka ganjil ================")
+
+	for i := 0; i <= 50; i++ {
+		if i%2 != 0 {
+			println(i)
+		}
+	}
+
+	println("================ angka ganjil dan genap ================")
+
+	for i := 0; i <= 50; i++ {
+		if i%2 == 0 {
+			println(i, "genap")
+		} else {
+			println(i, "ganjil")
+		}
+	}
+}
