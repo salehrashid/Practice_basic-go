4401cab070fd91e75b7bd968d83e8bf2da808f3f
salehrashid <saleh.rashid.b@gmail.com>
2023-09-02 17:13:01 +0700

    add function parameter

---
+++ functions/function-parameter.go
@@ -0,0 +1,9 @@
+package main
+
+func main() {
+	introduction("Saleh", "Rashid", 19)
+}
+
+func introduction(firstName string, lastName string, age int) {
+	println("Watashi no namae wa", firstName, lastName, "desu,", "Watashi no nenrei", age, "Yoroshiku onegai shimasu.")
+}
