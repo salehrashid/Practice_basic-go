6b7a2fe038549ae5e32e4e8d57ace4826ea67261
salehrashid <saleh.rashid.b@gmail.com>
2023-10-12 14:40:11 +0700

    add json decoder

---
+++ json/json-decode.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type User struct {
+	FullName string `json:"name"`
+	Age      int
+}
+
+func main() {
+	jsonString := `{"Name": "Saleh","Age": 19}`
+	jsonData := []byte(jsonString)
+
+	var data User
+
+	err := json.Unmarshal(jsonData, &data)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	fmt.Println("user :", data.FullName)
+	fmt.Println("age  :", data.Age)
+}
